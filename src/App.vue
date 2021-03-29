58193203281365ef67a24f65079adf70d5c4f342
mzgkswx <mizugaki@serverworks.co.jp>
2021-03-29 19:19:10 +0900

    MOD: ベースレイアウト調整

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,12 @@
   <div id="app">
     <router-view/>
     <div id="nav">
-      <router-link to="/">大沢</router-link> |
+      <router-link to="/">大沢</router-link>
       <router-link to="/area2">水附</router-link>
     </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./css/main.css";
 </style>
